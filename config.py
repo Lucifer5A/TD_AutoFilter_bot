39c7e2a034dd7a19df9d128adc26bc2eef0324d9
tdbotdev-source <tdbotdev@gmail.com>
2026-04-05 21:17:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,8 +5,8 @@ API_HASH = "fa3796dbdec1efdf151aca5f14815d06"
 BOT_TOKEN = "8579277980:AAHwYFeP1rLzDbPSZ9-98lsqSdgXMkQMJ98"
 
 # Search Settings
-db_CHANNEL_ID = -1002598623129
-START_TEXT = "Send any movie or file name to search 🔍"
+db_CHANNEL_ID = -1003101356980
+START_TEXT = "🚀 Welcome to TDBotDev Ultra\n\n🎬 Movies • Series • Anime • Episodes — All in One Place\n⚡ Lightning Fast Search with Smart Auto Filters\n🧠 Auto Detect Language • Quality • Seasons\n\n📂 Continue Watching • 🔥 Trending • ▶️ One Click Play\n\n🔍 Just send any name (movie / series / file) to start exploring"
 MAX_RESULTS = 10
 
 # Database Settings
